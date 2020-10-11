31adfab80c0838b52eca6ca1ead1e8b34b9cb04e
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-10-11 16:17:16 -0500

    added and styled Navbar

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Nav, Navbar } from 'react-bootstrap';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar className="Navbar">
+      <Nav>
+        <Nav.Link href="#home" style={{color: "white", paddingLeft: "10px", fontSize: "50px"}}>Jack Mitchell</Nav.Link>
+        <Nav.Link href="#home">Home</Nav.Link> | 
+        <Nav.Link href="#contact">Contact</Nav.Link> | 
+        <Nav.Link href="#blog">Blog</Nav.Link> | 
+        <Nav.Link href="#projects" style={{paddingRight: "0px"}}>Projects</Nav.Link>
+      </Nav>
+    </Navbar>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
