991572d99c5b354593b6f7cb662d9e139ab53c8d
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-12-12 15:12:13 -0600

    added contact information

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App" style={{"fontSize": "22px"}}>
+      <div id="home">
+        <h1 className="Head-Line">Hi, I'm Jack Mitchell!</h1>
+      </div>
+
+      <div id="contact">
+        <h2>Contact Jack Mitchell</h2>
+        <p>Phone Number:</p>
+        <a href="tel:5127880140">512-788-0140</a>
+        <p>Email:</p>
+        <a href="mailto:jack.danielle.mitchell@gmail.com">jack.danielle.mitchell@gmail.com</a>
+        <p>LinkedIn:</p>
+        <a href="http://www.linkedin.com/in/jack-danielle-mitchell">linkedin.com/in/jack-danielle-mitchell</a>
+        <p>GitHub:</p>
+        <a href="http://www.github.com/daphinora">github.com/daphinora</a>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,18 +6,21 @@ import * as serviceWorker from './serviceWorker';
 import { Nav, Navbar } from 'react-bootstrap';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Navbar className="Navbar">
-      <Nav>
-        <Nav.Link href="#home" className="Name">Jack Mitchell</Nav.Link>
-        <Nav.Link href="#home">Home</Nav.Link> |
-        <Nav.Link href="#contact">Contact</Nav.Link> |
-        <Nav.Link href="#blog">Blog</Nav.Link> |
-        <Nav.Link href="#projects" style={{ paddingRight: "0px" }}>Projects</Nav.Link>
-      </Nav>
-    </Navbar>
-    <App />
-  </React.StrictMode>,
+  <div className="Index">
+    <React.StrictMode>
+      <Navbar className="Navbar">
+        <Nav>
+          <Nav.Link href="#home" className="Name">JACK MITCHELL</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link> |
+          <Nav.Link href="#contact">Contact</Nav.Link> |
+          <Nav.Link href="#blog">Blog</Nav.Link> |
+          <Nav.Link href="#projects" style={{ paddingRight: "0px" }}>Projects</Nav.Link>
+        </Nav>
+      </Navbar>
+      <App />
+      {/* <img className="Image" src="https://images.unsplash.com/photo-1563089145-599997674d42?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" /> */}
+    </React.StrictMode>
+  </div>,
   document.getElementById('root')
 );
 
