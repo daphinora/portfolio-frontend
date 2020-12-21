8c7f74b5f49dc0cc16d08698732d89b4a155c2eb
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-12-21 11:33:45 -0600

    separated sections into components for easier traversing; fiddled with styling for About section

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../index.css';
+
+const About = () => {
+    return (
+        <div id="about" className="About">
+            {/* Who am I? Example here */}
+            <h1 className="Head-Line">Hi, I'm Jack Mitchell!</h1>
+            <p>I'm an aspiring Full Stack Software Developer located in Austin, Texas.</p>
+            <p>Learning is my passion!</p>
+        </div>
+    );
+}
+
+export default About;
